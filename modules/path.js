9e1a28aa59a80368849e17807b2b9815fd32b5a8
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-09-11 23:34:55 +0900

    Module path

---
+++ modules/path.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+// basename
+// console.log(path.basename(__filename))
+
+// dirname
+// console.log(path.dirname(__filename))
+
+// extname
+// console.log(path.extname(__filename))
+
+// parse
+// console.log(path.parse(__filename))
+
+// join
+console.log(path.join(__dirname, "templates", "index.html"))
