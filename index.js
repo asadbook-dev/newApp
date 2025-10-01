eebcb7df46bdcc2838b59b36c7d795bfaddfee5a
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-02 00:31:36 +0900

    web server

--- index.js
+++ index.js
@@ -10,16 +10,41 @@
 
 const http = require("http")
 
-const server = http.createServer((request, response) => {
+const server = http.createServer((req, res) => {
 
     // request - serverga so'rov
     // response - server javob
 
-    console.log(request.url)
+    // console.log(request.url)
 
-    response.write('<h1>Hello world 3</h1>')
+    // response.write('<h1>Hello world 3</h1>')
 
-    response.end()
+    // response.end()
+
+
+    if(req.method === "GET"){
+        res.writeHead(200, {"Content-Type": "text/html"})
+        res.end(`
+            <h3>Send name</h3>
+            <form method="post" action="/">
+                <input name="name" placeholder="Enter your name"/>
+                <button type="submit">Send name</button>
+            </form>
+        `)
+    } else if(req.method === "POST"){
+        const body = []
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on("end", () => {
+            const message = body.toString().split("=")[1]
+
+            res.end(`Name successfully added: ${message}`)
+        })
+
+        
+    }
 
 
 })
