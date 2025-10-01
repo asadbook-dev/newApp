7309801aa945f87383afbca2d598161d6d813ec2
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-01 23:28:03 +0900

    Module http

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // import userData from "./user";
 // import carData from "./car"
 
-const userData = require("./user")
-const carData = require("./car")
+// const userData = require("./user")
+// const carData = require("./car")
+
+// console.log(userData)   
+// console.log(carData);
+// console.log(carData.carLogger());
+
+const http = require("http")
+
+const server = http.createServer((request, response) => {
+
+    // request - serverga so'rov
+    // response - server javob
+
+    console.log(request.url)
+
+    response.write('<h1>Hello world</h1>')
+
+    response.end()
 
 
+})
 
-console.log(userData)   
-console.log(carData);
-
-console.log(carData.carLogger());
+server.listen(3000, () => {
+    console.log("Server has been started on port 3000")
+})
